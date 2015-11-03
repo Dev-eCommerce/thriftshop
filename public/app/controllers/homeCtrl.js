c6b2183828ab5af1aeebd76e3580102cb4f4328e
stevetalbot <steve_t221@yahoo.com>
2015-11-03 10:38:23 -0700

    added basic structure for account modal

--- public/app/controllers/homeCtrl.js
+++ public/app/controllers/homeCtrl.js
@@ -1,6 +1,7 @@
 var eCommerce = angular.module('eCommerce');
 
 eCommerce.controller('homeCtrl', function($scope, $location, homeService){
+    
 	$scope.createUser = function(user){
 		homeService.createUser(user).then(function(resp) {
 			// $location.path('/user/' + resp.data._id)
@@ -8,6 +9,9 @@ eCommerce.controller('homeCtrl', function($scope, $location, homeService){
 			return err;
 		});
 	}
+    
+    $scope.accountModal = "#myLogin";
+    
 })
 
 
