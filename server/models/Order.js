14c06cd8310527808484b5ba8888f5903d449b63
Machias F <machiasf@Machias-Fs-MacBook-Pro.local>
2015-11-04 11:31:54 -0700

    server fix

--- server/models/Order.js
+++ server/models/Order.js
@@ -7,7 +7,7 @@ var Order = new mongoose.Schema({
     shippingCost: {type: Number},
     orderTax: {type: Number},
     orderDate: {type: Date},
-    orderStatus: [{type: String, enum: ['Cart', 'Submitted', 'Fulfilled', 'Shipped']},
+    orderStatus: [{type: String, enum: ['Cart', 'Submitted', 'Fulfilled', 'Shipped']}],
     shipDate: {type: Date},
     trackingNumber: {type: String},
     productsOrdered: [{
