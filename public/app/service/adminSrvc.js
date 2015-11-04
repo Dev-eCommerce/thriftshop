50184452b83cc3e25921b0ce437543ea16330d4b
Todd Hayes <todd@i.tv>
2015-11-04 15:49:54 -0700

    GET all Products and basic POST Products Now working

--- public/app/service/adminSrvc.js
+++ public/app/service/adminSrvc.js
@@ -13,4 +13,32 @@ app.service('adminSrvc', function($http){
 			return response.data;
 		});
 	}
+	
+	this.getProducts = function(){
+		return $http({
+			method: 'GET',
+			url: '/api/products'
+		}).then(function(response){
+			if (response.status != 200) {
+				return "Orders not found";
+			}
+			console.log(response.data);
+			return response.data;
+		});
+	}
+	
+	this.addProduct = function(product){
+		product.options = {optionName: product.options.optionName, optionValues: [{count: product.options.optionValues.count, name: product.options.optionValues.name}]}
+		return $http({
+			method: 'POST',
+			url: '/api/products',
+			data: product
+		}).then(function(response){
+			if (response.status != 200) {
+				return "Orders not found";
+			}
+			console.log(response);
+			return response;
+		})
+	}
 })
--- server/controllers/ProductsCtrl.js
+++ server/controllers/ProductsCtrl.js
@@ -33,16 +33,16 @@ module.exports = {
                           	res.json(result);
                         }
                     });
-                    if (!req.body.image){
-                        console.log(req.body);
-                        Products.create(req.body, function(err, result) {
-                            if (err) {
-                             	res.send(err, "user not created");
-                            } else {
-                              	res.json(result);
-                            }
-                        });
-                    }  
+                    // if (!req.body.image){
+                    //     console.log(req.body);
+                    //     Products.create(req.body, function(err, result) {
+                    //         if (err) {
+                    //          	res.send(err, "user not created");
+                    //         } else {
+                    //           	res.json(result);
+                    //         }
+                    //     });
+                    // }  
             //     }
             // })
     },
