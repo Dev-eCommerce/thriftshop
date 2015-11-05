6f907f15243b57454741412f7a681da1ee16c4ae
Arun Selvaraj <zeezey@Aruns-MBP.home>
2015-11-04 19:29:16 -0700

    changing checkout page

--- public/app/controllers/productCtrl.js
+++ public/app/controllers/productCtrl.js
@@ -1,10 +1,24 @@
 var eCommerce = angular.module('eCommerce');
-eCommerce.controller('productCtrl', function($scope, $location, productService){
-	$scope.findProduct = function(){
-		productService.findAll().then(function(resp) {
-            return resp;
-		}, function(err) {
-			return err;
-		});
-	};
+eCommerce.controller("productCtrl", function($scope, $http) {
+  $http.get('api/products').
+    success(function(data, status, headers, config) {
+      $scope.products = data;
+    }).
+    error(function(data, status, headers, config) {
+      // log error
+    });
 });
+
+
+//}controller('productCtrl', function($scope, $location, productService){
+//	$scope.findProduct = function(){
+//		productService.findAll().then(function(resp) {
+//            
+//            console.log('111', resp);
+//            return resp;
+//            
+//		}, function(err) {
+//			return err;
+//		});
+//	};
+//});
