b59fc90bb46491b5570546ab119f5c96622f02ce
Arun Selvaraj <zeezey@Aruns-MacBook-Pro.local>
2015-11-05 14:17:19 -0700

    blah

--- public/app/service/productSrv.js
+++ public/app/service/productSrv.js
@@ -1,20 +1,20 @@
 var eCommerce = angular.module('eCommerce');
 
-//eCommerce.service('productService', function($http){
-//	this.findAll = function(products){
-//		return $http({
-//			method: "GET",
-//			url: 'api/products',
-//			data: products
-//
-//		}).then(function(err, res){
-//			if(err){
-//				console.log(err);
-//				return(err);
-//			} else {
-//				console.log(res);
-//				return res;
-//			}
-//		})
-//	}
-//});
+eCommerce.service('productService', function($http){
+	this.findAll = function(products){
+		return $http({
+			method: "GET",
+			url: 'api/products',
+			data: products
+
+		}).then(function(err, res){
+			if(err){
+				console.log(err);
+				return(err);
+			} else {
+				console.log(res);
+				return res;
+			}
+		})
+	}
+});
