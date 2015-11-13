2f11fe9253faaf390f281cc3932ac77395509a62
Todd Hayes <todd@i.tv>
2015-11-13 16:21:46 -0700

    route filter working

--- public/app/controllers/productCtrl.js
+++ public/app/controllers/productCtrl.js
@@ -1,22 +1,11 @@
 var eCommerce = angular.module('eCommerce');
 eCommerce.controller("productCtrl", function($scope, $location, $http, $state, $stateParams, productService, getCart) {
     $scope.cart = getCart;
-    var getProducts = productService.getProducts()
-			.then(function(response){
-                console.log('products', response)
-				$scope.products = response;
-                $scope.tops = [];
-                response.forEach(function(item){
-                    var category = item.category;
-                    switch(category){
-                        case 'tops':
-                        $scope.tops.push(category)
-                        break;
-                        default:
-                        alert("non found");
-                    }
-                })
-            });
+    // var getProducts = productService.getProducts()
+	// 		.then(function(response){
+    //             console.log('products', response)
+	// 			$scope.products = response;
+    //             })
     
     $scope.addToCart = function(product) {
         productService.addToCart(product).then(function(response){
