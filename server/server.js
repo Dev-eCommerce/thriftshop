7ac77903ebd996042e4ee4b7d92ea99088c63ca9
Todd Hayes <todd@i.tv>
2015-11-13 13:02:11 -0700

    fixed controller issue

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ var config = require('../config');
 var UsersCtrl = require('./controllers/UsersCtrl');
 var ProductsCtrl = require('./controllers/ProductsCtrl');
 var OrdersCtrl= require('./controllers/OrdersCtrl');
-var MandrillCtrl = require('./controllers/mandrillCtrl')
+var MandrillCtrl = require('./controllers/ContactCtrl')
 
 // Services
 var passport = require('./services/passport');
